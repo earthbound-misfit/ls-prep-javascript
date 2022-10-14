d2e5ed20f0688090447289ec1082b69de5de2997
Kelly Karner <kellymkarner@gmail.com>
2022-10-14 18:57:53 -0500

    completed more exercises

---
+++ 1_basics/3.js
@@ -0,0 +1,11 @@
+/*
+
+Identify the data type for each of the following values:
+'true'         --> String
+false          --> Boolean
+1.5            --> Number - Float/Decimal
+2              --> Number - Integer
+undefined      --> Undefined
+{ foo: 'bar' } --> Object
+
+*/
