4da4c203c2ebbc7f6b4e8dae3ac727a508004c37
Kelly Karner <kellymkarner@gmail.com>
2022-10-15 16:35:27 -0500

    input/output exercises

---
+++ 3_input_output/1_greeter.js
@@ -0,0 +1,2 @@
+let f_name = prompt("What's your name?");
+console.log(`Hello, ${f_name}!`)
---
+++ 3_input_output/2_greeter.js
@@ -0,0 +1,3 @@
+let f_name = prompt("What's your first name?")
+let l_name = prompt("What's your last name?")
+console.log(`Hello, ${f_name} ${l_name}!`)
